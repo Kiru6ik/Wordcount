a68d39879ef0add3ef353087eac1fcc86d1be58a
Kiru6ik <116097426+Kiru6ik@users.noreply.github.com>
2023-07-11 11:34:15 -0400

    Add files via upload

---
+++ dads_wordcount.py
@@ -0,0 +1,45 @@
+import string
+import re
+import nltk
+from nltk import word_tokenize
+import csv
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+spec_chars = string.punctuation + "«»\t—…’"
+
+f = open('/Users/kirillevseev/Downloads/Книга на перевод.txt', "r", encoding="utf-8")
+
+text = f.read()
+text = text.lower()
+text = re.sub(r'http\S+', '', text)
+text = re.sub('isbn', '', text)
+text = re.sub('©', '', text)
+text = "".join([ch for ch in text if ch not in spec_chars])
+text = re.sub('\n', ' ', text)
+text = "".join([ch for ch in text if ch not in string.digits])
+
+text_tokens = word_tokenize(text)
+text = nltk.Text(text_tokens)
+stopwords=stopwords.words("english")
+stopwords.extend(['–', '•'])
+text_tokens = [token.strip() for token in text_tokens if token not in stopwords]
+text = nltk.Text(text_tokens)
+
+fdist = FreqDist(text)
+# only_once=FreqDist.hapaxes(text_tokens)
+words=fdist.most_common()
+with open('dads_words.csv', 'w', encoding='utf-8') as file:
+    writer=csv.writer(file)
+    writer.writerows(words)
+
+# text_raw = " ".join(text)
+# wordcloud = WordCloud(width=1600, height=800).generate(text_raw)
+# plt.figure( figsize=(20,10), facecolor='k')
+# plt.imshow(wordcloud)
+# plt.axis("off")
+# plt.tight_layout(pad=0)
+# plt.show()
+# print(text[:3000])
